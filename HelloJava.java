277fd5f61e93acbe54927306f824dd3fd26759e4
mexgas <mexicanlocos@gmail.com>
2022-09-19 20:40:15 -0500

    Provoca Conflicto

--- HelloJava.java
+++ HelloJava.java
@@ -3,7 +3,8 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java2");
-		  
+		System.out.println("Merge");
+		System.out.println("Merge Java2");
 		  
 	
 
