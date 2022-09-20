da4b9e5e35ef8fb81a4159571f73ab6b0e556698
mexgas <mexicanlocos@gmail.com>
2022-09-19 20:38:46 -0500

    Cambios

--- HelloJava.java
+++ HelloJava.java
@@ -2,9 +2,9 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello Java2");
-		  
-		  
+		System.out.println("Hello Java2"); //branch
+		System.out.println("Hello Java2"); //branch
+		System.out.println("Hello Java2"); //branch
 	
 
 	}
